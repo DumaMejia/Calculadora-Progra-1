6363ea87ca4ab61976bec26ba7a960d708a52817
Duma Roberto Zelaya Mejia <usis007420@ugb.edu.sv>
2020-08-19 16:39:10 -0600

    Convenrsor

--- holaMundo/Form1.Designer.vb
+++ holaMundo/Form1.Designer.vb
@@ -22,55 +22,13 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Texto = New System.Windows.Forms.Label()
-        Me.Tablas1 = New System.Windows.Forms.ListBox()
-        Me.Ctexto = New System.Windows.Forms.TextBox()
-        Me.Boton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
-        '
-        'Texto
-        '
-        Me.Texto.AutoSize = True
-        Me.Texto.Location = New System.Drawing.Point(86, 81)
-        Me.Texto.Name = "Texto"
-        Me.Texto.Size = New System.Drawing.Size(56, 20)
-        Me.Texto.TabIndex = 0
-        Me.Texto.Text = "Tabla: "
-        '
-        'Tablas1
-        '
-        Me.Tablas1.FormattingEnabled = True
-        Me.Tablas1.ItemHeight = 20
-        Me.Tablas1.Location = New System.Drawing.Point(280, 81)
-        Me.Tablas1.Name = "Tablas1"
-        Me.Tablas1.Size = New System.Drawing.Size(359, 244)
-        Me.Tablas1.TabIndex = 1
-        '
-        'Ctexto
-        '
-        Me.Ctexto.Location = New System.Drawing.Point(139, 81)
-        Me.Ctexto.Name = "Ctexto"
-        Me.Ctexto.Size = New System.Drawing.Size(100, 26)
-        Me.Ctexto.TabIndex = 2
-        '
-        'Boton
-        '
-        Me.Boton.Location = New System.Drawing.Point(102, 128)
-        Me.Boton.Name = "Boton"
-        Me.Boton.Size = New System.Drawing.Size(137, 66)
-        Me.Boton.TabIndex = 3
-        Me.Boton.Text = "Generar tabla"
-        Me.Boton.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(696, 403)
-        Me.Controls.Add(Me.Boton)
-        Me.Controls.Add(Me.Ctexto)
-        Me.Controls.Add(Me.Tablas1)
-        Me.Controls.Add(Me.Texto)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.MaximizeBox = False
@@ -78,12 +36,6 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.Text = "tablas de duma :U"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Texto As Label
-    Friend WithEvents Tablas1 As ListBox
-    Friend WithEvents Ctexto As TextBox
-    Friend WithEvents Boton As Button
 End Class
--- holaMundo/Form1.vb
+++ holaMundo/Form1.vb
@@ -1,19 +1,3 @@
 ﻿Public Class Form1
-    Private Sub Boton_Click(sender As Object, e As EventArgs) Handles Boton.Click
-        Dim Tablas, i As SByte
-        Tablas = Ctexto.Text
 
-        Tablas1.Items.Clear()
-        If Tablas <= 12 Then
-            For i = 1 To 10
-                Tablas1.Items.Add(Tablas.ToString() + "x" + i.ToString() + "=" + (Tablas * i).ToString())
-            Next
-        Else
-            Tablas1.Items.Add("Por Favor  ingrese  solo tablas menores o iguales a 12")
-        End If
-    End Sub
-
-    Private Sub Tablas1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Tablas1.SelectedIndexChanged
-
-    End Sub
 End Class
