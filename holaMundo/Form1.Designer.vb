9694c19fc75ecd6cdd7f268daf5dc589803befa8
Duma Roberto Zelaya Mejia <usis007420@ugb.edu.sv>
2020-08-20 17:49:52 -0600

    Conversor v2

--- holaMundo/Form1.Designer.vb
+++ holaMundo/Form1.Designer.vb
@@ -22,13 +22,95 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Caja = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Respuesta = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Monedas", "Masa", "Volumen", "Longitud", "Almacenamiento", "Tiempo"})
+        Me.ComboBox1.Location = New System.Drawing.Point(223, 61)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 28)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(429, 184)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(121, 28)
+        Me.ComboBox3.TabIndex = 1
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(96, 184)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 28)
+        Me.ComboBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(120, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Convertir"
+        '
+        'Caja
+        '
+        Me.Caja.Location = New System.Drawing.Point(244, 187)
+        Me.Caja.Name = "Caja"
+        Me.Caja.Size = New System.Drawing.Size(100, 26)
+        Me.Caja.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(385, 188)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(18, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "a"
+        '
+        'Respuesta
+        '
+        Me.Respuesta.Location = New System.Drawing.Point(450, 268)
+        Me.Respuesta.Name = "Respuesta"
+        Me.Respuesta.Size = New System.Drawing.Size(100, 26)
+        Me.Respuesta.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(278, 299)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(113, 44)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(696, 403)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Respuesta)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Caja)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.ComboBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.MaximizeBox = False
@@ -36,6 +118,16 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.Text = "tablas de duma :U"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Caja As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Respuesta As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- holaMundo/Form1.vb
+++ holaMundo/Form1.vb
@@ -1,3 +1,44 @@
 ﻿Public Class Form1
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Select Case ComboBox1.SelectedIndex
+            Case 0
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("prueba")
+                ComboBox2.Items.Add("1")
+                ComboBox2.Items.Add("1")
+                ComboBox2.Items.Add("1")
+                ComboBox2.Items.Add("1")
+                ComboBox2.Items.Add("1")
+                ComboBox2.Items.Add("1")
+                ComboBox2.Items.Add("1")
+                ComboBox2.Items.Add("1")
+                ComboBox2.Items.Add("1")
 
+                ComboBox3.Items.Add("ejemplo")
+                ComboBox3.Items.Add("1")
+                ComboBox3.Items.Add("1")
+                ComboBox3.Items.Add("1")
+                ComboBox3.Items.Add("1")
+                ComboBox3.Items.Add("1")
+                ComboBox3.Items.Add("1")
+                ComboBox3.Items.Add("1")
+                ComboBox3.Items.Add("1")
+                ComboBox3.Items.Add("1")
+                ComboBox3.Items.Add("1")
+            Case 1
+                ComboBox3.Items.Add("1")
+                ComboBox3.Items.Add("1")
+
+        End Select
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Select Case ComboBox2.SelectedIndex
+            Case 0
+                Dim Array() = {1, 2, 3, 4, 5}, Valor = Caja.Text
+                If ComboBox3.SelectedIndex = 0 Then
+                    Respuesta.Text = Valor * Array(0)
+                End If
+        End Select
+    End Sub
 End Class
