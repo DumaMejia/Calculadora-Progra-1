b5b9a16afefdcba9753c0d2b8f4920321726d384
Roberto Carlos Hernandez Melendez <usis016520@ugb.edu.sv>
2020-08-21 18:00:33 -0600

    Conversor V.2.1

--- holaMundo/Form1.vb
+++ holaMundo/Form1.vb
@@ -3,42 +3,364 @@
         Select Case ComboBox1.SelectedIndex
             Case 0
                 ComboBox2.Items.Clear()
-                ComboBox2.Items.Add("prueba")
-                ComboBox2.Items.Add("1")
-                ComboBox2.Items.Add("1")
-                ComboBox2.Items.Add("1")
-                ComboBox2.Items.Add("1")
-                ComboBox2.Items.Add("1")
-                ComboBox2.Items.Add("1")
-                ComboBox2.Items.Add("1")
-                ComboBox2.Items.Add("1")
-                ComboBox2.Items.Add("1")
+                ComboBox2.Items.Add("Dolar Estado Unidense")
+                ComboBox2.Items.Add("Peso Mexicano")
+                ComboBox2.Items.Add("Euro")
+                ComboBox2.Items.Add("Dolar Canadience")
+                ComboBox2.Items.Add("Peso Chileno")
+                ComboBox2.Items.Add("Colon Salvadoreño")
+                ComboBox2.Items.Add("Lempira")
+                ComboBox2.Items.Add("Libra Esterlina")
+                ComboBox2.Items.Add("Peso Colombiano")
+                ComboBox2.Items.Add("Peso Argentino")
 
-                ComboBox3.Items.Add("ejemplo")
-                ComboBox3.Items.Add("1")
-                ComboBox3.Items.Add("1")
-                ComboBox3.Items.Add("1")
-                ComboBox3.Items.Add("1")
-                ComboBox3.Items.Add("1")
-                ComboBox3.Items.Add("1")
-                ComboBox3.Items.Add("1")
-                ComboBox3.Items.Add("1")
-                ComboBox3.Items.Add("1")
-                ComboBox3.Items.Add("1")
+                ComboBox3.Items.Clear()
+                ComboBox3.Items.Add("Dolar Estado Unidense")
+                ComboBox3.Items.Add("Peso Mexicano")
+                ComboBox3.Items.Add("Euro")
+                ComboBox3.Items.Add("Dolar Canadience")
+                ComboBox3.Items.Add("Peso Chileno")
+                ComboBox3.Items.Add("Colon Salvadoreño")
+                ComboBox3.Items.Add("Lempira")
+                ComboBox3.Items.Add("Libra Esterlina")
+                ComboBox3.Items.Add("Peso Colombiano")
+                ComboBox3.Items.Add("Peso Argentino")
             Case 1
-                ComboBox3.Items.Add("1")
-                ComboBox3.Items.Add("1")
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("Kilogramo")
+                ComboBox2.Items.Add("Gramo")
+                ComboBox2.Items.Add("Miligramo")
+                ComboBox2.Items.Add("Microgramo")
+                ComboBox2.Items.Add("Tonelada larga")
+                ComboBox2.Items.Add("Tonelada corta")
+                ComboBox2.Items.Add("Stone")
+                ComboBox2.Items.Add("Libra")
+                ComboBox2.Items.Add("Onza")
+                ComboBox2.Items.Add("Tonelada")
+
+                ComboBox3.Items.Clear()
+                ComboBox3.Items.Add("Kilogramo")
+                ComboBox3.Items.Add("Gramo")
+                ComboBox3.Items.Add("Miligramo")
+                ComboBox3.Items.Add("Microgramo")
+                ComboBox3.Items.Add("Tonelada larga")
+                ComboBox3.Items.Add("Tonelada corta")
+                ComboBox3.Items.Add("Stone")
+                ComboBox3.Items.Add("Libra")
+                ComboBox3.Items.Add("Onza")
+                ComboBox3.Items.Add("Tonelada")
+
+            Case 2
+
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("Metro cubico")
+                ComboBox2.Items.Add("Litro Cubico")
+                ComboBox2.Items.Add("Mililitro")
+                ComboBox2.Items.Add("Galon imperial")
+                ComboBox2.Items.Add("Cuarto Imperial")
+                ComboBox2.Items.Add("Pinta imperial")
+                ComboBox2.Items.Add("Taza Imperial")
+                ComboBox2.Items.Add("Onza Liquida")
+                ComboBox2.Items.Add("Pie Cubico")
+                ComboBox2.Items.Add("Pulgada Cubica")
+
+                ComboBox3.Items.Clear()
+                ComboBox3.Items.Add("Metro cubico")
+                ComboBox3.Items.Add("Litro Cubico")
+                ComboBox3.Items.Add("Mililitro")
+                ComboBox3.Items.Add("Galon imperial")
+                ComboBox3.Items.Add("Cuarto Imperial")
+                ComboBox3.Items.Add("Pinta imperial")
+                ComboBox3.Items.Add("Taza Imperial")
+                ComboBox3.Items.Add("Onza Liquida")
+                ComboBox3.Items.Add("Pie Cubico")
+                ComboBox3.Items.Add("Pulgada Cubica")
+
+            Case 3
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("Kilometro")
+                ComboBox2.Items.Add("Metro")
+                ComboBox2.Items.Add("Centimetro")
+                ComboBox2.Items.Add("Milimetro")
+                ComboBox2.Items.Add("Micrometro")
+                ComboBox2.Items.Add("Nanometro")
+                ComboBox2.Items.Add("Milla")
+                ComboBox2.Items.Add("Yarda")
+                ComboBox2.Items.Add("Pie")
+                ComboBox2.Items.Add("Pulgada")
+
+                ComboBox3.Items.Clear()
+                ComboBox3.Items.Add("Kilometro")
+                ComboBox3.Items.Add("Metro")
+                ComboBox3.Items.Add("Centimetro")
+                ComboBox3.Items.Add("Milimetro")
+                ComboBox3.Items.Add("Micrometro")
+                ComboBox3.Items.Add("Nanometro")
+                ComboBox3.Items.Add("Milla")
+                ComboBox3.Items.Add("Yarda")
+                ComboBox3.Items.Add("Pie")
+                ComboBox3.Items.Add("Pulgada")
+
+            Case 4
+
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("Bit")
+                ComboBox2.Items.Add("Byte")
+                ComboBox2.Items.Add("Kilobyte")
+                ComboBox2.Items.Add("Megabyte")
+                ComboBox2.Items.Add("Gigabyte")
+                ComboBox2.Items.Add("Terabyte")
+                ComboBox2.Items.Add("Petabyte")
+                ComboBox2.Items.Add("Exabyte")
+                ComboBox2.Items.Add("Zettabyte")
+                ComboBox2.Items.Add("Yottabyte")
+
+                ComboBox3.Items.Clear()
+                ComboBox3.Items.Add("Bit")
+                ComboBox3.Items.Add("Byte")
+                ComboBox3.Items.Add("Kilobyte")
+                ComboBox3.Items.Add("Megabyte")
+                ComboBox3.Items.Add("Gigabyte")
+                ComboBox3.Items.Add("Terabyte")
+                ComboBox3.Items.Add("Petabyte")
+                ComboBox3.Items.Add("Exabyte")
+                ComboBox3.Items.Add("Zettabyte")
+                ComboBox3.Items.Add("Yottabyte")
+
+            Case 5
+
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("Milisegundo")
+                ComboBox2.Items.Add("Segundo")
+                ComboBox2.Items.Add("Minuto")
+                ComboBox2.Items.Add("Hora")
+                ComboBox2.Items.Add("Dia")
+                ComboBox2.Items.Add("Semana")
+                ComboBox2.Items.Add("Mes")
+                ComboBox2.Items.Add("Año")
+                ComboBox2.Items.Add("Decada ")
+                ComboBox2.Items.Add("Siglo")
+
+                ComboBox3.Items.Clear()
+                ComboBox3.Items.Add("Milisegundo")
+                ComboBox3.Items.Add("Segundo")
+                ComboBox3.Items.Add("Minuto")
+                ComboBox3.Items.Add("Hora")
+                ComboBox3.Items.Add("Dia")
+                ComboBox3.Items.Add("Semana")
+                ComboBox3.Items.Add("Mes")
+                ComboBox3.Items.Add("Año")
+                ComboBox3.Items.Add("Decada ")
+                ComboBox3.Items.Add("Siglo")
 
         End Select
     End Sub
 
+    Dim Array()
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Select Case ComboBox2.SelectedIndex
+        Dim Valor = Caja.Text
+        Dim Selec = ComboBox3.SelectedIndex
+        Select Case ComboBox1.SelectedIndex
             Case 0
-                Dim Array() = {1, 2, 3, 4, 5}, Valor = Caja.Text
-                If ComboBox3.SelectedIndex = 0 Then
-                    Respuesta.Text = Valor * Array(0)
-                End If
+                Select Case ComboBox2.SelectedIndex
+                    Case 0
+                        Array = {1, 21.98, 0.85, 1.32, 793.1, 8.75, 24.66, 0.76, 3836, 73.59}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 1
+                        Array = {3.35, 1, 0.039, 0.06, 36.09, 0.4, 1.12, 0.035, 174.76, 3.35}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 2
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 3
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 4
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 5
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 6
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 7
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 8
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 9
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                End Select
+
+            Case 1
+                Select Case ComboBox2.SelectedIndex
+                    Case 0
+                        Array = {1, 21.98, 0.85, 1.32, 793.1, 8.75, 24.66, 0.76, 3836, 73.59}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 1
+                        Array = {3.35, 1, 0.039, 0.06, 36.09, 0.4, 1.12, 0.035, 174.76, 3.35}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 2
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 3
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 4
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 5
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 6
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 7
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 8
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 9
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                End Select
+            Case 2
+                Select Case ComboBox2.SelectedIndex
+                    Case 0
+                        Array = {1, 21.98, 0.85, 1.32, 793.1, 8.75, 24.66, 0.76, 3836, 73.59}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 1
+                        Array = {3.35, 1, 0.039, 0.06, 36.09, 0.4, 1.12, 0.035, 174.76, 3.35}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 2
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 3
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 4
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 5
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 6
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 7
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 8
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 9
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                End Select
+            Case 3
+                Select Case ComboBox2.SelectedIndex
+                    Case 0
+                        Array = {1, 21.98, 0.85, 1.32, 793.1, 8.75, 24.66, 0.76, 3836, 73.59}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 1
+                        Array = {3.35, 1, 0.039, 0.06, 36.09, 0.4, 1.12, 0.035, 174.76, 3.35}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 2
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 3
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 4
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 5
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 6
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 7
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 8
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 9
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                End Select
+            Case 4
+                Select Case ComboBox2.SelectedIndex
+                    Case 0
+                        Array = {1, 21.98, 0.85, 1.32, 793.1, 8.75, 24.66, 0.76, 3836, 73.59}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 1
+                        Array = {3.35, 1, 0.039, 0.06, 36.09, 0.4, 1.12, 0.035, 174.76, 3.35}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 2
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 3
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 4
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 5
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 6
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 7
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 8
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 9
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                End Select
+            Case 5
+                Select Case ComboBox2.SelectedIndex
+                    Case 0
+                        Array = {1, 21.98, 0.85, 1.32, 793.1, 8.75, 24.66, 0.76, 3836, 73.59}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 1
+                        Array = {3.35, 1, 0.039, 0.06, 36.09, 0.4, 1.12, 0.035, 174.76, 3.35}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 2
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 3
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 4
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 5
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 6
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 7
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 8
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                    Case 9
+                        Array = {2, 3, 4, 5}
+                        Respuesta.Text = Valor * Array(Selec)
+                End Select
         End Select
     End Sub
 End Class
