b05f373e15775aab6cc9ffff062ef6898bff0724
Duma Roberto Zelaya Mejia <usis007420@ugb.edu.sv>
2020-09-03 13:03:31 -0600

    Parcial1

--- holaMundo/Form1.vb
+++ holaMundo/Form1.vb
@@ -1,20 +1,31 @@
 ﻿Public Class Form1
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim Uni = unidades.Text
         Dim Res = respuesta.Text
+        Dim Resp
+        Dim Resp2
         Dim Can = cantidad.Text
         Dim NoIntRes
         Dim SubIn
+        Dim CnSuma
+        Dim x
+
+
 
         If respuesta.Text = "" Then
-            Res = Can / Uni
-            NoIntRes = Int(Res)
-            SubIn = (NoIntRes * Uni) - Can
-            respuesta.Text = NoIntRes + "/" + SubIn
+            Resp = (Can / Uni)
+            NoIntRes = Int(Resp)
+            SubIn = Can - (NoIntRes * Uni)
+            respuesta.Text = NoIntRes.ToString() + "/" + SubIn.ToString()
         End If
         If cantidad.Text = "" Then
-            cantidad.Text = Res * Uni
+            Resp2 = respuesta.Text.Substring(0, 1)
+            x = respuesta.Text.Substring(2)
+            CnSuma = Resp2 * Uni + x
+            cantidad.Text = CnSuma
         End If
+
     End Sub
 End Class
 
