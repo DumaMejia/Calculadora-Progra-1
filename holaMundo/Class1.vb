5828bb3f8854db12352d8e1633c7f2a8c599b1b2
Duma Roberto Zelaya Mejia <usis007420@ugb.edu.sv>
2020-10-05 23:26:19 -0600

    PROTO

--- holaMundo/Class1.vb
+++ holaMundo/Class1.vb
@@ -1,3 +1,28 @@
-﻿Public Class Class1
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Public Class db_conexion
+    Dim miconexion As New SqlConnection
+    Dim micomand As New SqlCommand
+    Dim miadapter As New SqlDataAdapter
+    Dim ds As DataSet
+
+    Public Sub New()
+        Dim c_conexion As String
+        c_conexion = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DBClinica.mdf;Integrated Security=True"
+        miconexion.ConnectionString = c_conexion
+
+        miconexion.Open()
+    End Sub
+
+    Public Function obtenerdatos()
+        ds.Clear()
+        micomand.Connection = miconexion
+
+        micomand.CommandText = "select * from medicina"
+        miadapter.SelectCommand = micomand
+        miadapter.Fill(ds, "medicina")
+        Return ds
+    End Function
+
 
 End Class
--- holaMundo/Form1.vb
+++ holaMundo/Form1.vb
@@ -1,4 +1,17 @@
 ﻿Public Class Form1
+    Dim conexion As New db_conexion
+    Dim datatable As New DataTable
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        obtenerdatos()
+    End Sub
+    Sub obtenerdatos()
+        datatable = conexion.obtenerdatos().Tables("medicina")
+
+        mostrardatos()
+    End Sub
+    Sub mostrardatos()
+        MessageBox.Show("Nombre: " + datatable.Rows(1).ItemArray(1).ToString())
+    End Sub
 
 End Class
 
