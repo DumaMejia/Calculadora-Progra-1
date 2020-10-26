636202ddd7a759198a6d103c7d2b6dd4cffa2486
Jose Roberto del Rio Maravilla <usis015220@ugb.edu.sv>
2020-10-25 18:25:52 -0600

    5 formularios basicos funcionales

---
+++ holaMundo/alergia.Designer.vb
@@ -0,0 +1,292 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class alergia
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PanelBD = New System.Windows.Forms.Panel()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Idalergia = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.descripcion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.tratamiento = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PanelDatos = New System.Windows.Forms.Panel()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.PanelBuscarDiag = New System.Windows.Forms.Panel()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.modificarBT = New System.Windows.Forms.Button()
+        Me.nuevoBT = New System.Windows.Forms.Button()
+        Me.eliminarBT = New System.Windows.Forms.Button()
+        Me.PanelBD.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelDatos.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PanelBD
+        '
+        Me.PanelBD.Controls.Add(Me.DataGridView1)
+        Me.PanelBD.Controls.Add(Me.TextBox3)
+        Me.PanelBD.Controls.Add(Me.Label4)
+        Me.PanelBD.Location = New System.Drawing.Point(410, 27)
+        Me.PanelBD.Margin = New System.Windows.Forms.Padding(2)
+        Me.PanelBD.Name = "PanelBD"
+        Me.PanelBD.Size = New System.Drawing.Size(458, 266)
+        Me.PanelBD.TabIndex = 58
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Idalergia, Me.nombre, Me.descripcion, Me.tratamiento})
+        Me.DataGridView1.Location = New System.Drawing.Point(31, 76)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(414, 172)
+        Me.DataGridView1.TabIndex = 45
+        '
+        'Idalergia
+        '
+        Me.Idalergia.DataPropertyName = "Idalergia"
+        Me.Idalergia.HeaderText = "IDALER"
+        Me.Idalergia.MinimumWidth = 8
+        Me.Idalergia.Name = "Idalergia"
+        Me.Idalergia.ReadOnly = True
+        Me.Idalergia.Visible = False
+        Me.Idalergia.Width = 150
+        '
+        'nombre
+        '
+        Me.nombre.DataPropertyName = "nombre"
+        Me.nombre.HeaderText = "NOMBRE"
+        Me.nombre.MinimumWidth = 8
+        Me.nombre.Name = "nombre"
+        Me.nombre.ReadOnly = True
+        Me.nombre.Width = 150
+        '
+        'descripcion
+        '
+        Me.descripcion.DataPropertyName = "descripcion"
+        Me.descripcion.HeaderText = "DESCRIPCION"
+        Me.descripcion.MinimumWidth = 8
+        Me.descripcion.Name = "descripcion"
+        Me.descripcion.ReadOnly = True
+        Me.descripcion.Width = 150
+        '
+        'tratamiento
+        '
+        Me.tratamiento.DataPropertyName = "tratamiento"
+        Me.tratamiento.FillWeight = 200.0!
+        Me.tratamiento.HeaderText = "TRATAMIENTO"
+        Me.tratamiento.MinimumWidth = 8
+        Me.tratamiento.Name = "tratamiento"
+        Me.tratamiento.ReadOnly = True
+        Me.tratamiento.Width = 150
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(91, 38)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(355, 20)
+        Me.TextBox3.TabIndex = 44
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 41)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 13)
+        Me.Label4.TabIndex = 42
+        Me.Label4.Text = "BUSCAR:"
+        '
+        'PanelDatos
+        '
+        Me.PanelDatos.Controls.Add(Me.TextBox4)
+        Me.PanelDatos.Controls.Add(Me.PanelBuscarDiag)
+        Me.PanelDatos.Controls.Add(Me.TextBox2)
+        Me.PanelDatos.Controls.Add(Me.TextBox1)
+        Me.PanelDatos.Controls.Add(Me.Label3)
+        Me.PanelDatos.Controls.Add(Me.Label2)
+        Me.PanelDatos.Controls.Add(Me.Label1)
+        Me.PanelDatos.Location = New System.Drawing.Point(11, 27)
+        Me.PanelDatos.Margin = New System.Windows.Forms.Padding(2)
+        Me.PanelDatos.Name = "PanelDatos"
+        Me.PanelDatos.Size = New System.Drawing.Size(394, 216)
+        Me.PanelDatos.TabIndex = 57
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(171, 159)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox4.TabIndex = 20
+        '
+        'PanelBuscarDiag
+        '
+        Me.PanelBuscarDiag.Location = New System.Drawing.Point(397, -6)
+        Me.PanelBuscarDiag.Margin = New System.Windows.Forms.Padding(2)
+        Me.PanelBuscarDiag.Name = "PanelBuscarDiag"
+        Me.PanelBuscarDiag.Size = New System.Drawing.Size(387, 403)
+        Me.PanelBuscarDiag.TabIndex = 19
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(171, 73)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(126, 74)
+        Me.TextBox2.TabIndex = 17
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(171, 26)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox1.TabIndex = 16
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(81, 162)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(85, 13)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "TRATAMIENTO"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(88, 76)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "DESCRIPCION"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(88, 26)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "NOMBRE"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(312, 247)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 28)
+        Me.Button1.TabIndex = 56
+        Me.Button1.Text = "Buscar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'modificarBT
+        '
+        Me.modificarBT.Location = New System.Drawing.Point(102, 247)
+        Me.modificarBT.Margin = New System.Windows.Forms.Padding(2)
+        Me.modificarBT.Name = "modificarBT"
+        Me.modificarBT.Size = New System.Drawing.Size(75, 28)
+        Me.modificarBT.TabIndex = 55
+        Me.modificarBT.Text = "Modificar"
+        Me.modificarBT.UseVisualStyleBackColor = True
+        '
+        'nuevoBT
+        '
+        Me.nuevoBT.Location = New System.Drawing.Point(11, 247)
+        Me.nuevoBT.Margin = New System.Windows.Forms.Padding(2)
+        Me.nuevoBT.Name = "nuevoBT"
+        Me.nuevoBT.Size = New System.Drawing.Size(75, 28)
+        Me.nuevoBT.TabIndex = 54
+        Me.nuevoBT.Text = "Nuevo"
+        Me.nuevoBT.UseVisualStyleBackColor = True
+        '
+        'eliminarBT
+        '
+        Me.eliminarBT.Location = New System.Drawing.Point(206, 247)
+        Me.eliminarBT.Margin = New System.Windows.Forms.Padding(2)
+        Me.eliminarBT.Name = "eliminarBT"
+        Me.eliminarBT.Size = New System.Drawing.Size(75, 28)
+        Me.eliminarBT.TabIndex = 53
+        Me.eliminarBT.Text = "Eliminar"
+        Me.eliminarBT.UseVisualStyleBackColor = True
+        '
+        'alergia
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(879, 321)
+        Me.Controls.Add(Me.PanelBD)
+        Me.Controls.Add(Me.PanelDatos)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.modificarBT)
+        Me.Controls.Add(Me.nuevoBT)
+        Me.Controls.Add(Me.eliminarBT)
+        Me.Name = "alergia"
+        Me.Text = "alergia"
+        Me.PanelBD.ResumeLayout(False)
+        Me.PanelBD.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelDatos.ResumeLayout(False)
+        Me.PanelDatos.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelBD As Panel
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PanelDatos As Panel
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents PanelBuscarDiag As Panel
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents modificarBT As Button
+    Friend WithEvents nuevoBT As Button
+    Friend WithEvents eliminarBT As Button
+    Friend WithEvents Idalergia As DataGridViewTextBoxColumn
+    Friend WithEvents nombre As DataGridViewTextBoxColumn
+    Friend WithEvents descripcion As DataGridViewTextBoxColumn
+    Friend WithEvents tratamiento As DataGridViewTextBoxColumn
+End Class
--- holaMundo/menu.Designer.vb
+++ holaMundo/menu.Designer.vb
@@ -30,11 +30,12 @@ Partial Class menu
         Me.ENFERMEDADESToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.DIAGNOSTICOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.RECETAToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TRASLADOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.PEROSNALToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.CARGOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.USUARIOSToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.HORARIOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.TRASLADOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ALERGIAToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -44,8 +45,8 @@ Partial Class menu
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PACIENTESToolStripMenuItem, Me.PACIENTEToolStripMenuItem, Me.PEROSNALToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(5, 2, 0, 2)
-        Me.MenuStrip1.Size = New System.Drawing.Size(1053, 28)
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(4, 2, 0, 2)
+        Me.MenuStrip1.Size = New System.Drawing.Size(790, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -53,85 +54,91 @@ Partial Class menu
         '
         Me.PACIENTESToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MEDICAMENTOSToolStripMenuItem, Me.PROVEEDORToolStripMenuItem})
         Me.PACIENTESToolStripMenuItem.Name = "PACIENTESToolStripMenuItem"
-        Me.PACIENTESToolStripMenuItem.Size = New System.Drawing.Size(93, 24)
+        Me.PACIENTESToolStripMenuItem.Size = New System.Drawing.Size(75, 20)
         Me.PACIENTESToolStripMenuItem.Text = "MEDICINA"
         '
         'MEDICAMENTOSToolStripMenuItem
         '
         Me.MEDICAMENTOSToolStripMenuItem.Name = "MEDICAMENTOSToolStripMenuItem"
-        Me.MEDICAMENTOSToolStripMenuItem.Size = New System.Drawing.Size(205, 26)
+        Me.MEDICAMENTOSToolStripMenuItem.Size = New System.Drawing.Size(164, 22)
         Me.MEDICAMENTOSToolStripMenuItem.Text = "MEDICAMENTOS"
         '
         'PROVEEDORToolStripMenuItem
         '
         Me.PROVEEDORToolStripMenuItem.Name = "PROVEEDORToolStripMenuItem"
-        Me.PROVEEDORToolStripMenuItem.Size = New System.Drawing.Size(205, 26)
+        Me.PROVEEDORToolStripMenuItem.Size = New System.Drawing.Size(164, 22)
         Me.PROVEEDORToolStripMenuItem.Text = "PROVEEDOR"
         '
         'PACIENTEToolStripMenuItem
         '
-        Me.PACIENTEToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ENFERMEDADESToolStripMenuItem, Me.DIAGNOSTICOToolStripMenuItem, Me.RECETAToolStripMenuItem, Me.TRASLADOToolStripMenuItem})
+        Me.PACIENTEToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ENFERMEDADESToolStripMenuItem, Me.DIAGNOSTICOToolStripMenuItem, Me.RECETAToolStripMenuItem, Me.TRASLADOToolStripMenuItem, Me.ALERGIAToolStripMenuItem})
         Me.PACIENTEToolStripMenuItem.Name = "PACIENTEToolStripMenuItem"
-        Me.PACIENTEToolStripMenuItem.Size = New System.Drawing.Size(88, 24)
+        Me.PACIENTEToolStripMenuItem.Size = New System.Drawing.Size(71, 20)
         Me.PACIENTEToolStripMenuItem.Text = "PACIENTE"
         '
         'ENFERMEDADESToolStripMenuItem
         '
         Me.ENFERMEDADESToolStripMenuItem.Name = "ENFERMEDADESToolStripMenuItem"
-        Me.ENFERMEDADESToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.ENFERMEDADESToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.ENFERMEDADESToolStripMenuItem.Text = "ENFERMEDADES"
         '
         'DIAGNOSTICOToolStripMenuItem
         '
         Me.DIAGNOSTICOToolStripMenuItem.Name = "DIAGNOSTICOToolStripMenuItem"
-        Me.DIAGNOSTICOToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.DIAGNOSTICOToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.DIAGNOSTICOToolStripMenuItem.Text = "DIAGNOSTICO"
         '
         'RECETAToolStripMenuItem
         '
         Me.RECETAToolStripMenuItem.Name = "RECETAToolStripMenuItem"
-        Me.RECETAToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.RECETAToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.RECETAToolStripMenuItem.Text = "RECETA"
+        '
+        'TRASLADOToolStripMenuItem
+        '
+        Me.TRASLADOToolStripMenuItem.Name = "TRASLADOToolStripMenuItem"
+        Me.TRASLADOToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.TRASLADOToolStripMenuItem.Text = "TRASLADO"
         '
         'PEROSNALToolStripMenuItem
         '
         Me.PEROSNALToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CARGOToolStripMenuItem, Me.USUARIOSToolStripMenuItem, Me.HORARIOToolStripMenuItem})
         Me.PEROSNALToolStripMenuItem.Name = "PEROSNALToolStripMenuItem"
-        Me.PEROSNALToolStripMenuItem.Size = New System.Drawing.Size(95, 24)
+        Me.PEROSNALToolStripMenuItem.Size = New System.Drawing.Size(77, 20)
         Me.PEROSNALToolStripMenuItem.Text = "PERSONAL"
         '
         'CARGOToolStripMenuItem
         '
         Me.CARGOToolStripMenuItem.Name = "CARGOToolStripMenuItem"
-        Me.CARGOToolStripMenuItem.Size = New System.Drawing.Size(162, 26)
+        Me.CARGOToolStripMenuItem.Size = New System.Drawing.Size(129, 22)
         Me.CARGOToolStripMenuItem.Text = "CARGO"
         '
         'USUARIOSToolStripMenuItem
         '
         Me.USUARIOSToolStripMenuItem.Name = "USUARIOSToolStripMenuItem"
-        Me.USUARIOSToolStripMenuItem.Size = New System.Drawing.Size(162, 26)
+        Me.USUARIOSToolStripMenuItem.Size = New System.Drawing.Size(129, 22)
         Me.USUARIOSToolStripMenuItem.Text = "USUARIOS"
         '
         'HORARIOToolStripMenuItem
         '
         Me.HORARIOToolStripMenuItem.Name = "HORARIOToolStripMenuItem"
-        Me.HORARIOToolStripMenuItem.Size = New System.Drawing.Size(162, 26)
+        Me.HORARIOToolStripMenuItem.Size = New System.Drawing.Size(129, 22)
         Me.HORARIOToolStripMenuItem.Text = "HORARIO"
         '
-        'TRASLADOToolStripMenuItem
+        'ALERGIAToolStripMenuItem
         '
-        Me.TRASLADOToolStripMenuItem.Name = "TRASLADOToolStripMenuItem"
-        Me.TRASLADOToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
-        Me.TRASLADOToolStripMenuItem.Text = "TRASLADO"
+        Me.ALERGIAToolStripMenuItem.Name = "ALERGIAToolStripMenuItem"
+        Me.ALERGIAToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ALERGIAToolStripMenuItem.Text = "ALERGIA"
         '
         'menu
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1053, 565)
+        Me.ClientSize = New System.Drawing.Size(790, 459)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
-        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "menu"
         Me.Text = "menu"
         Me.MenuStrip1.ResumeLayout(False)
@@ -154,4 +161,5 @@ Partial Class menu
     Friend WithEvents RECETAToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents HORARIOToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents TRASLADOToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ALERGIAToolStripMenuItem As ToolStripMenuItem
 End Class
